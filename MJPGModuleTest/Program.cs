a99d619d85eb6779383c3ad02be8bc9c30eee9c6
Ben Cox <Ben@Benjojo.co.uk>
2013-09-21 14:58:02 +0100

    I *may* have forgotten how much I hate events in C#

---
+++ MJPGModuleTest/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MJPGModuleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- MJPGSplitter/Class1.cs
+++ MJPGSplitter/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 
 namespace MJPGSplitter
 {
@@ -14,14 +15,51 @@ namespace MJPGSplitter
      */
     public class MJPEGDecode
     {
-        private List<byte> Buffer = new List<byte>();
+        private byte[] Buffer = new byte[500 * 1024]; // 500k buffer.
+        private int BufferPointer = 0;
+        public delegate void NewImageHandler(object sender, EventArgs data);
 
         public void GiveData(byte[] Inbound)
         {
             foreach (byte Input in Inbound)
             {
-                Buffer.Add(Input);
+                Buffer[BufferPointer] = Input;
+                BufferPointer++;
             }
         }
+        private int threshold;
+        private int total;
+
+        public MJPEGDecode()
+        {
+
+        }
+
+        public void DecodeAndFlush(int x)
+        {
+
+            NewImageEventArgs args = new NewImageEventArgs();
+            args.DecodedOutput = new Bitmap(1,1);
+            OnNewImage(args);
+        }
+
+
+        protected virtual void OnNewImage(NewImageEventArgs e)
+        {
+            NewImageEventHandler handler = NewImage;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        public event NewImageEventHandler NewImage;
     }
+
+    public class NewImageEventArgs : EventArgs
+    {
+        public Bitmap DecodedOutput;
+    }
+
+    public delegate void NewImageEventHandler(Object sender, NewImageEventArgs e);
 }
