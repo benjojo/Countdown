aa4af9345ca28d51f61c09eeff49f7af857e41d7
Ben Cox <Ben@Benjojo.co.uk>
2013-09-21 15:17:07 +0100

    MJPEG Decoder done?

--- MJPGSplitter/Class1.cs
+++ MJPGSplitter/Class1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+//using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 
 namespace MJPGSplitter
 {
@@ -15,6 +16,11 @@ namespace MJPGSplitter
      */
     public class MJPEGDecode
     {
+        public MJPEGDecode()
+        {
+
+        }
+        private byte[] JPEGHeader = new byte[12] {0xFF,0xD9,0xFF,0xD8,0xFF,0xE0,0x00,0x10,0x4A,0x46,0x49,0x46};
         private byte[] Buffer = new byte[500 * 1024]; // 500k buffer.
         private int BufferPointer = 0;
         public delegate void NewImageHandler(object sender, EventArgs data);
@@ -25,22 +31,71 @@ namespace MJPGSplitter
             {
                 Buffer[BufferPointer] = Input;
                 BufferPointer++;
+                CheckForJPEGHeaders();
             }
         }
+
+        public void CheckForJPEGHeaders()
+        {
+            int track = 0;
+            for (int i = BufferPointer - 12; i < BufferPointer; i++)
+            {
+                if (Buffer[BufferPointer] != JPEGHeader[track])
+                {
+                    return;
+                }
+                track++;
+            }
+            //oshit call the cops
+            DecodeAndFlush(BufferPointer - 12);
+        }
+
         private int threshold;
         private int total;
 
-        public MJPEGDecode()
-        {
 
+
+        public byte[] ExtractFromArray(int stoppoint)
+        {
+            byte[] SendBack = new byte[stoppoint];
+            // JPEG.
+
+            for (int i = 0; i < stoppoint; i++)
+            {
+                SendBack[i] = Buffer[i];
+            }
+
+            // Clear the buffer
+            for (int i = 0; i < Buffer.Length; i++)
+            {
+                SendBack[i] = 0x00;
+            }
+
+            // Put the JPEG header back in the buffer
+            for (int i = 0; i < JPEGHeader.Length; i++)
+            {
+                Buffer[i] = JPEGHeader[i];
+            }
+            BufferPointer = JPEGHeader.Length;
+
+            return SendBack;
         }
 
         public void DecodeAndFlush(int x)
         {
-
-            NewImageEventArgs args = new NewImageEventArgs();
-            args.DecodedOutput = new Bitmap(1,1);
-            OnNewImage(args);
+            // Get the byte array of JPEG
+            try
+            {
+                MemoryStream JPEGRAW = new MemoryStream(ExtractFromArray(BufferPointer - 12));
+                Image MaybeJPEG = Image.FromStream(JPEGRAW); // I have no idea if its gonna be able to do this.
+                NewImageEventArgs args = new NewImageEventArgs();
+                args.DecodedOutput = new Bitmap(MaybeJPEG);
+                OnNewImage(args);
+            }
+            catch
+            {
+                //NOTHING BAD EVER HAPPENED.
+            }
         }
 
 
