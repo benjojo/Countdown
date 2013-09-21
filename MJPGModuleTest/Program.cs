a09344e8354a40421622a17d235844e2bb54ca0b
Ben Cox <Ben@Benjojo.co.uk>
2013-09-21 15:24:57 +0100

    Test Application to test MJPEG decoder

--- MJPGModuleTest/Program.cs
+++ MJPGModuleTest/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Drawing;
+using System.IO;
+using MJPGSplitter;
 
 namespace MJPGModuleTest
 {
@@ -6,6 +9,23 @@ namespace MJPGModuleTest
     {
         static void Main(string[] args)
         {
+            MJPEGDecode Decoder = new MJPEGDecode();
+            Decoder.NewImage += c_TestImage;
+            using (Stream stdin = Console.OpenStandardInput())
+            {
+                byte[] buffer = new byte[12];
+                int bytes;
+                while ((bytes = stdin.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    Decoder.GiveData(buffer);
+                }
+            }
+        }
+
+        static void c_TestImage(Object sender, NewImageEventArgs e)
+        {
+            Console.WriteLine("I've got a image!!1");
+            Environment.Exit(0);
         }
     }
 }
