d5e016811381aa2a0487e3233c7a402b502626a4
Ben Cox <Ben@Benjojo.co.uk>
2013-09-21 15:56:21 +0100

    Decoder done, Now working on the main application itself

--- AutoCountdown/Program.cs
+++ AutoCountdown/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Drawing;
+using MJPGSplitter;
 
 namespace AutoCountdown
 {
@@ -6,7 +9,16 @@ namespace AutoCountdown
     {
         static void Main(string[] args)
         {
-
+            MJPEGDecode Decoder = new MJPEGDecode();
+            using (Stream stdin = Console.OpenStandardInput())
+            {
+                byte[] buffer = new byte[12];
+                int bytes;
+                while ((bytes = stdin.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    Decoder.GiveData(buffer);
+                }
+            }
         }
     }
 }
