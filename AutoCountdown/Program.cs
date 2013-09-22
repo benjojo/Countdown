c0d8ee23ae189b216775bff7df072e2c316c9b33
Ben Cox <ben@benjojo.co.uk>
2013-09-22 17:13:22 +0100

    Added cropping func

--- AutoCountdown/Program.cs
+++ AutoCountdown/Program.cs
@@ -43,6 +43,48 @@ namespace AutoCountdown
             decodecount++;
         }
 
+        static Bitmap CropToText(Bitmap Input)
+        {
+            int Top = 272;
+            int Bot = Input.Height;
+            int Mid = Bot - ((Bot - Top) / 2);
+            Color Target = Color.FromArgb(255, 49, 76, 153);
+
+            int Lx = 0;
+            while (ColTestWTolerance(Input.GetPixel(Lx,Mid), Target, 15))
+            {
+                Lx++;
+            }
+
+
+            int Rx = Input.Width -1;
+            while (ColTestWTolerance(Input.GetPixel(Rx, Mid), Target, 15))
+            {
+                Rx--;
+            }
+
+            Console.WriteLine("Cropping to {0},{1} {2},{3}");
+
+            Bitmap Returner = new Bitmap(Mid + 200, Bot - 272);
+            int Rxx = 0;
+            int Ry = 0;
+            for (int Cx = Lx; Cx < Rx; Cx++)
+            {
+                for (int Cy = 272; Cy < Bot - 1; Cy++)
+                {
+                    if (ColTestWTolerance(Input.GetPixel(Cx, Cy), Color.FromArgb(255, 255, 255, 255), 70))
+                        Returner.SetPixel(Rxx, Ry, Color.FromArgb(255, 0, 0, 0));
+                    else
+                        Returner.SetPixel(Rxx, Ry, Color.FromArgb(255, 255, 255, 255));
+
+                    
+                    Ry++;
+                }
+                Ry = 0;
+                Rxx++;
+            }
+            return Returner;
+        }
         static bool FrameCycle(Bitmap Output,Color Target, int Tolerance,int Shift)
         {
             for (int n = 0; n < 50; n = n + 5)
