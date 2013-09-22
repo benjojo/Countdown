f7ff14e13cc935e962c26577f6c7e542b006998a
Ben Cox <ben@benjojo.co.uk>
2013-09-22 14:31:06 +0100

    Added a less derpy check

--- AutoCountdown/Program.cs
+++ AutoCountdown/Program.cs
@@ -32,30 +32,30 @@ namespace AutoCountdown
             return;
             Bitmap Output = e.DecodedOutput;
             // First test to see if its the frame we want.
-            Color Target = Color.FromArgb(255, 51, 76, 157);
-            int Tolerance = 5;
+            Color Target = Color.FromArgb(255, 49, 76, 153);
+            int Tolerance = 10;
             for (int i = 267; i < Output.Height; i++)
             {
                 // Move the way down though the image to check.
                 Color Pixel = Output.GetPixel(10, i);
-                if(!ColTestWTolerance(Pixel, Tolerance))
+                if(!ColTestWTolerance(Pixel, Target , Tolerance))
                     return;
             }
             Console.WriteLine("We seem to have a frame that matches what we want.");
         }
 
 
-        static bool ColTestWTolerance(Color Target, int Tolerance)
+        static bool ColTestWTolerance(Color Input, Color Target, int Tolerance)
         {
-            if (NumberTol(Target.R, Tolerance) && NumberTol(Target.G, Tolerance) && NumberTol(Target.B, Tolerance))
+            if (NumberTol(Input.R, Target.R, Tolerance) && NumberTol(Input.G, Target.G, Tolerance) && NumberTol(Input.B, Target.B, Tolerance))
                 return true;
             else
                 return false;
         }
 
-        static bool NumberTol(int target, int tol)
+        static bool NumberTol(int input, int target, int tol)
         {
-            if (target - tol > target && target < target+tol)
+            if (target - tol > input && input < target + tol)
             {
                 return true;
             }
